0641f792a40d49e2fca54ba4542bbe0acaee896e
tt-a1i <tt-a1i@users.noreply.github.com>
2026-08-22 19:15:50 +0800

    chore: pin formula to Windows evidence fix

--- Formula/skillroster.rb
+++ Formula/skillroster.rb
@@ -2,7 +2,7 @@ class Skillroster < Formula
   desc "Local skill governance for AI agents"
   homepage "https://github.com/tt-a1i/skillroster"
   url "https://github.com/tt-a1i/skillroster.git",
-      revision: "bff308ad5b5c3e059253ed0adb869d15098b8344"
+      revision: "b814c9fa14966f6e36013a5cae9d95cb36ce10a3"
   version "1.5.1"
 
   depends_on "rust" => :build
