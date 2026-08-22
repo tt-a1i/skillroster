399a423cd9deb724983df47704173be47261d49e
tt-a1i <tt-a1i@users.noreply.github.com>
2026-08-22 15:44:56 +0800

    chore: point formula at 1.0.4 source

--- Formula/skillroster.rb
+++ Formula/skillroster.rb
@@ -2,8 +2,8 @@ class Skillroster < Formula
   desc "Local skill governance for AI agents"
   homepage "https://github.com/tt-a1i/skillroster"
   url "https://github.com/tt-a1i/skillroster.git",
-      revision: "c142d98941fd32122ab7db893a5122aad7f29878"
-  version "1.0.3"
+      revision: "32de61249238904c344409592a17202dcaf88077"
+  version "1.0.4"
 
   depends_on "rust" => :build
 
@@ -12,7 +12,7 @@ class Skillroster < Formula
   end
 
   test do
-    assert_match "skillroster 1.0.3", shell_output("#{bin}/skillroster --version")
+    assert_match "skillroster 1.0.4", shell_output("#{bin}/skillroster --version")
     assert_match "One library. The right roster for every agent.", shell_output("#{bin}/skillroster --help")
   end
 end
