a7c93116a08e67180a3e9b24219e4180f63ab0e0
tt-a1i <tt-a1i@users.noreply.github.com>
2026-08-23 04:13:34 +0800

    chore: pin formula to finding rollups

--- Formula/skillroster.rb
+++ Formula/skillroster.rb
@@ -2,8 +2,8 @@ class Skillroster < Formula
   desc "Local skill governance for AI agents"
   homepage "https://github.com/tt-a1i/skillroster"
   url "https://github.com/tt-a1i/skillroster.git",
-      revision: "97b6e03da5bf9f7e501b849f61e3af81e43c6161"
-  version "1.8.8"
+      revision: "c60fabc640507355923b791601178901252a9eb4"
+  version "1.8.9"
 
   depends_on "rust" => :build
 
@@ -12,7 +12,7 @@ class Skillroster < Formula
   end
 
   test do
-    assert_match "skillroster 1.8.8", shell_output("#{bin}/skillroster --version")
+    assert_match "skillroster 1.8.9", shell_output("#{bin}/skillroster --version")
     assert_match "One library. The right roster for every agent.", shell_output("#{bin}/skillroster --help")
   end
 end
