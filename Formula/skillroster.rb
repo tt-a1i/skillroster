ecf85f1e664912c6b6e6286af39a7bceff3f1653
tt-a1i <tt-a1i@users.noreply.github.com>
2026-08-25 06:06:31 +0800

    release: prepare SkillRoster v1.8.27

--- Formula/skillroster.rb
+++ Formula/skillroster.rb
@@ -2,8 +2,8 @@ class Skillroster < Formula
   desc "Local skill governance for AI agents"
   homepage "https://github.com/tt-a1i/skillroster"
   url "https://github.com/tt-a1i/skillroster.git",
-      revision: "21f9e57e5af5f9cff9b3684bc48555f93020bfca"
-  version "1.8.26"
+      revision: "d1babf8c96de0436e38aaf8e9cdf168f14a33a80"
+  version "1.8.27"
   license "Apache-2.0"
 
   depends_on "rust" => :build
@@ -13,7 +13,7 @@ class Skillroster < Formula
   end
 
   test do
-    assert_match "skillroster 1.8.26", shell_output("#{bin}/skillroster --version")
+    assert_match "skillroster 1.8.27", shell_output("#{bin}/skillroster --version")
     assert_match "One library. The right roster for every agent.", shell_output("#{bin}/skillroster --help")
   end
 end
